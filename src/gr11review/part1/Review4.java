eea8081c179f66b4b71a8eac39a1bf6994b930a1
addisonc1 <addison.chan21@ycdsbk12.ca>
2020-11-25 05:05:37 +0000

    review 4

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,1 +1,31 @@
 package gr11review.part1;
+import java.io.*;
+import java.text.DecimalFormat;
+/**
+* a program that asks the user how many items he/she would like to purchase. The program then gets the prices for the items one by one. At the end of entering items, print out the total, the tax (13%) and the grand total.
+*@author: Addison Chan
+*/
+public class Review4{
+  public static void main (String[] args)throws Exception{
+    double dblamount;
+    double dblprice;
+    double dblsubtotal;
+    double dbltax;
+    double dbltotal;
+    int intcount;
+    dblprice =0;
+
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("how many items do you want to buy?")
+    dblamount = Double.parseDouble(keyboard.readLine());
+    for (dblamount == intcount){
+      System.out.println("Enter the price for the item"+intcount);
+      dblprice = Double.parseDouble
+    }
+    System.out.println("Subtotal"+dblprice)
+    dbltax = dblprice * 0.13;
+    System.out.println("tax:"+dbltax)
+    dbltotal = dblprice + dbltax
+    System.out.println("total:"+dbltotal)
+  }
+}
--- src/gr11review/part1/Review6.java
+++ src/gr11review/part1/Review6.java
@@ -1,1 +1,17 @@
 package gr11review.part1;
+import java.io.*;
+import java.text.DecimalFormat;
+/**
+* a cash register program similar to the one above, except instead of prompting for the number items,*the user is able to keep entering item prices until the enter 0
+*@author: Addison Chan
+*/
+public class Review6{
+  public static void main (String[] args)throws Exception{
+    double dblitem;
+    double dblsubtotal;
+    double dbltax;
+    double dbltotal;
+    System.out
+
+  }
+}
