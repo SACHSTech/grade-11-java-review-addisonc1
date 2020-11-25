bde8830c97181e233ba767e7340f8ec172c0ad0b
addisonc1 <addison.chan21@ycdsbk12.ca>
2020-11-25 00:18:07 +0000

    1 and 2

--- src/gr11review/part1/Review1.java
+++ src/gr11review/part1/Review1.java
@@ -1,1 +1,22 @@
 package gr11review.part1;
+import java.io.*;
+/**
+* that asks the user to enter a month number (1 = January, 2 = February etc.) and day and
+* then prints the day of the year. Assume that it is not a leap year.
+*@author: Addison Chan
+*/
+public class Review1 {
+  public static void main(String[] args)throws Exception{
+    // variables
+    int intmonth;
+    int intday;
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+    intmonth = 0;
+    intday = 0;
+    // get the days and months
+    System.out.println("What month do you want to print?");
+    intmonth = Integer.parseInt(keyboard.readLine());
+    //("what day of the month do you want to print?");
+     
+  }
+}
--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -1,1 +1,18 @@
 package gr11review.part1;
+import java.io.*;
+/**
+* Allow the user to select an option and react appropriately to the option selection using 
+* a switch case structure.
+*@author: Addison Chan
+*/
+public class Review2{
+  public static void main (String[] args)throws Exception{
+    int intchoice;
+    intchoice = 0;
+    System.out.println("Choose a choice");
+    System.out.println("0 - print an joke about your hair");
+    System.out.println("1 - print a joke about your feet");
+    System.out.println("2 - print a joke about your clothes");
+    System.out.println("3 - print a joke about your teacher.");
+    intchoice = readint(); 
+ 
