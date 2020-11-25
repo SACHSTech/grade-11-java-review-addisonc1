6a61c0818048b519b96036195b437c67f3226f44
addisonc1 <addison.chan21@ycdsbk12.ca>
2020-11-25 02:09:56 +0000

    review2 again

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -15,24 +15,25 @@ public class Review2{
 	  System.out.println("3 - print a joke about your teacher");
 	  System.out.println("Choose an option");
 	  intchoice = Integer.parseInt(keyboard.readLine());e   
-         switch(intchoice){
-		      case 0:
-		  	    System.out.println("How does the man on the moon cut his hair?");
-			      System.out.println("Eclipes it!");
-			      break;
-		      case 1:
-			      System.out.println("If your nose runs and your feet smell you're built upside down");
-		      	break;
-	      	case 2:
-			      System.out.println("What kind of clothing do Karens wear?");
-			      System.out.println(" A lawsuit");
-			      break;
-	      	case 3:
-		      	System.out.println("Why were the teacher's eyes crossed?");
-			      System.out.println("She couldn't control her pupils!");
-			      break;
-	      	default :
-			      System.out.println("Invalid menu option");
+         
+      switch(intchoice){
+		    case 0:
+		  	  System.out.println("How does the man on the moon cut his hair?");
+			    System.out.println("Eclipes it!");
+			    break;
+		    case 1:
+			    System.out.println("If your nose runs and your feet smell you're built upside down");
+		      break;
+	      case 2:
+			    System.out.println("What kind of clothing do Karens wear?");
+			    System.out.println(" A lawsuit");
+			    break;
+	      case 3:
+		      System.out.println("Why were the teacher's eyes crossed?");
+			    System.out.println("She couldn't control her pupils!");
+			    break;
+	      default :
+			    System.out.println("Invalid menu option");
 			
 		}
 		
