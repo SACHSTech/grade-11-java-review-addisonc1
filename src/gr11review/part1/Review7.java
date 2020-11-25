ee2189fcb907dec32f8ca6e9dd4445c0d8232fbb
addisonc1 <addison.chan21@ycdsbk12.ca>
2020-11-25 21:35:34 +0000

    finished for loop for review 7

--- src/gr11review/part1/Review7.java
+++ src/gr11review/part1/Review7.java
@@ -7,13 +7,41 @@ import java.io.*;
 */
 public class Review7{
   public static void main (String[] args)throws Exception{
+    //variables
     String strthesentence; 
-    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("Enter the sentence");
-    strthesentence = keyboard.readLine();
-     
-     
+    int intcount;
+    int intacounter;
+    int intletters;
+    int intnospace;
+    int intspaces;
+    int intoddcounter;
+    //variables state
+    intspaces = 0;
+    intacounter = 0;
 
+    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+    //sentence
+    System.out.println("Enter the sentence");
+      strthesentence = keyboard.readLine();
+
+      intletters = strthesentence.length();
+      intnospace = strthesentence.replace(" ","").length();
+      intspaces = intletters - intnospace;
+      
+     for(intcount = 0; intcount < strthesentence.length(); intcount++){
+       if(strthesentence.charAt(intcount) == 'a'){
+        intacounter = intacounter + 1;
+       }
+
+
+     }
+     while(){
+
+     }
+     System.out.println("total characters in the sentence is "+ intletters);
+     System.out.println("total spaces in the sentence is "+ intspaces);
+     System.out.println("total letter a's in the sentence is "+ intacounter);
+    
 
 
 
