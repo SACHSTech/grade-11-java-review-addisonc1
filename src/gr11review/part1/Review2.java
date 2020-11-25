7099dca5b6b9f494412b6d967c45db8f5ec5a86d
addisonc1 <addison.chan21@ycdsbk12.ca>
2020-11-25 00:18:39 +0000

    2

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -15,4 +15,21 @@ public class Review2{
     System.out.println("2 - print a joke about your clothes");
     System.out.println("3 - print a joke about your teacher.");
     intchoice = readint(); 
+   
+         if (intchoice == 0){
+        System.out.println("How does the man on the moon cut his hair? Eclipes it!");
+        } else if (intchoice == 1){
+        System.out.println("If your nose runs and your feet smell you're built upside down");
+        } else if (intchoice == 2){
+        System.out.println("What kind of clothing do Karens wear? A lawsuit.");
+        } else if (intchoice == 3){
+        System.out.println("Why were the teacher's eyes crossed? A: She couldn't control her pupils!");
+        } else { 
+          System.out.println("Invalid menu option");
+       
+
+      }
+      
+  }
+}
  
