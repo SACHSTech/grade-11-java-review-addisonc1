ead70ceca878e01e08fbf12438d20ed82425c67c
addisonc1 <addison.chan21@ycdsbk12.ca>
2020-11-25 04:35:02 +0000

    done review 5

--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -1,5 +1,7 @@
 package gr11review.part1;
 import java.io.*;
+import java.text.DecimalFormat;
+
 /**
 * After the first year $100.00 has been invested and the interest is 2.5/100*100 = $2.50. 
 *Thus the amount of money after the first year is now $100.00 + $2.50 = $102.50.
@@ -12,24 +14,24 @@ public class Review5{
     double dbltargetamount;
     double dbltime;
     double dblinvest;
-    
+    DecimalFormat dcm = new DecimalFormat("#.##");
     BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-    dblyearlydepo = 0;
-    dblintrestrate = 0;
-    dbltargetamount = 0;
     dbltime = 0;
     dblinvest = 0;
+
     System.out.println("Enter the yearly invested amount:");
     dblyearlydepo = Double.parseDouble(keyboard.readLine());
     System.out.println("Enter the compound interest rate:");
     dblintrestrate = Double.parseDouble(keyboard.readLine());
     System.out.println("Enter the target amount:");
     dbltargetamount = Double.parseDouble(keyboard.readLine());
+
       while (dblinvest <= dbltargetamount){
-        dblinvest = dblintrestrate/dblyearlydepo * 100 ;
+        dblinvest = dblinvest + dblyearlydepo;
+        dblinvest = (dblintrestrate/100 * dblinvest) + dblinvest ;
         dbltime = dbltime + 1;
         
       }
-      System.out.println("The target amount will be earned in"+dbltime);
+      System.out.println("The target amount will be earned in "+dbltime +"years");
   }
 }
